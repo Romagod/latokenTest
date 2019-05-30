a5fdb8ec172153d2283e3bbe864784d46ea027f5
Nolarok <Nolarok@users.noreply.github.com>
2019-05-30 16:33:12 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  @import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,700&display=swap');
+  font-family: 'Fira Sans', sans-serif;
+  font-size: 14px;
+
+
+}
+</style>
